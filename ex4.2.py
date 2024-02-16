a0f2467a72609ac95c3f0366dd45fe86c5438574
Aidan <aidanhuang10@gmail.com>
2024-02-16 15:56:36 -0700

    Finished 4.2

--- ex4.2.py
+++ ex4.2.py
@@ -5,9 +5,8 @@ from matplotlib import pyplot as plt
 # 3) Inefficient search for sorted array (linear search)
 def inefficientSearch (array, key):
     for i in range(len(array)):
-        for j in range(len(array)):
-            if array[i] == key:
-                return i
+        if array[i] == key:
+            return i
     return -1 #If key is not found
 
 # Efficient Search for sorted array (binary search)
@@ -28,29 +27,19 @@ def efficientSearch (array, first, last, key):
 #5) 
 INPUT_SIZE = 1000
 ITERATIONS = 100
-NUMBER_OF_TIMES = 10
-
-randList = [x for x in range(INPUT_SIZE)]   #Makes a list of 1000 elements
 
 avg_efficient = []
 avg_inefficient = []
-for i in range(NUMBER_OF_TIMES):
-    target = randList[random.randint(0, INPUT_SIZE - 1)]   #Minus one to ensure the random number is guaranteed to be in the list
-    inefficientTime = timeit.timeit(lambda: inefficientSearch(randList, target), number=ITERATIONS)
-    avg_inefficient.append(inefficientTime)
+for i in range(ITERATIONS):
+    randList = [random.randint(0, INPUT_SIZE) for _ in range(INPUT_SIZE)]
+    randList.sort()
+    target = random.choice(randList)   
+    avg_inefficient.append(timeit.timeit(lambda: inefficientSearch(randList, target), number=1))
+    avg_efficient.append(timeit.timeit(lambda: efficientSearch(randList, 0, len(randList) - 1, target), number=1))
 
-    efficientTime = timeit.timeit(lambda: efficientSearch(randList, 0, len(randList) - 1, target), number=ITERATIONS)
-    avg_efficient.append(efficientTime)
-
-for x in range(NUMBER_OF_TIMES):
-    avg_efficient[x] /= NUMBER_OF_TIMES
-    avg_inefficient[x] /= NUMBER_OF_TIMES
-
-testRunNumber = [k + 1 for k in range(NUMBER_OF_TIMES)]
-
-plt.scatter(testRunNumber, avg_efficient, label='Binary Search')
-plt.scatter(testRunNumber, avg_inefficient, label='Linear Search')
-plt.xlabel('Test Number')
-plt.ylabel('Average Time (s)')
+plt.xlabel('Average Time (s)')
+plt.ylabel('Frequency')
+plt.hist(avg_efficient, bins=10, alpha=0.5, label='Binary Search')
+plt.hist(avg_inefficient, bins=10, alpha=0.4, label='Linear Search')
 plt.legend()
 plt.show()
