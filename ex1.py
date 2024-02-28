813921ae7e364d72dafa04cddc6b4082a99cea7c
Aidan <aidanhuang10@gmail.com>
2024-02-27 20:28:36 -0700

    I finished question 1

--- ex1.py
+++ ex1.py
@@ -1,6 +1,9 @@
 import timeit
 import random 
 from matplotlib import pyplot as plt
+import numpy as np
+import scipy
+
 # 1) Linked List Node Class
 class Node: 
     def __init__(self, data): 
@@ -45,63 +48,87 @@ def binarySearchList(head, value):
             break
     return None #Value Not Found
 
-# 3) 
+# 3) Array Class used to store integers
 class Array:
-    #Creates an array of length n with zeros
-    def __init__(self, size):
-        self.size = size
-        self.data = [0]*size
-
-    #For setting the values of an array using the index with the integer to put in
-    def set(self, index, value):
-        if 0 <= index < self.size:
-            self.data[index] = value
-        else:
-            raise IndexError("Index out of bounds")
-
+    def __init__(self, arr):
+        self.arr = arr
+    
+    def len(self):
+        return len(self.arr)
+    
+    def get(self, index):
+        return self.arr[index]
+    
 #Binary Search for Arrays O(log(n))
 def binarySearchArray (arr, first, last, key):
     if (first <= last):
         mid = (first + last) // 2
-        if(key == arr[mid]):
+        if(key == arr.get(mid)):
             return mid
-        elif(key < arr[mid]):
+        elif(key < arr.get(mid)):
             return binarySearchArray(arr, first, mid - 1, key)
-        elif(key > arr[mid]):
+        elif(key > arr.get(mid)):
             return binarySearchArray(arr, mid + 1, last, key)
     return -1 #Key was not found in the array
 
+#Makes a singly linked list of integers of the required size and returns the fully linked list
+def populateList(SIZE):
+    current = newNode(0)
+    head = current
+
+    for i in range(SIZE):
+        current.next = newNode(i + 1)
+        current = current.next
+
+    return head
+
+#Function for fitting logarithmic data curve fitting
+def func(x, a, b):
+    return a * np.log2(x) + b
+
 # 4) The complexity of binary search on linked list is O(n) due to the fact that there is no way of
-#    indexing each node for the linked list. For that reason, in order to perform binary search on 
+#    indexing each node for the linked list. This means we must traverse the whole list in order to find the middle element
+#    for a linked list. For that reason, in order to perform binary search on 
 #    a linked list, the worst case is for you to traverse through the list of 'n' nodes in 
-#    order to find the nth element. 
+#    order to find the nth element; in our case, its to find the middle element. 
 
 # 5)
 INPUT_SIZE = [1000, 2000, 4000, 8000]   #Number of elements for array and linked list
 ITERATIONS = 100    #Number of Iterations
 
+#List for average times calculated for each input size
+avgLinkedList = []
+avgArray = []
+
 for size in INPUT_SIZE:
-    print("HIU")
-    # linkedListBinary = timeit.repeat(lambda: LINKEDLIST(), repeat=ITERATIONS, number=1)
-    # arrayBinary = timeit.repeat(lambda: LINKEDLIST(), repeat=ITERATIONS, number=1)
+    #Uses class Array to store integers
+    randArray = Array([i for i in range(0, size)])
+    head = populateList(size)
 
-# popt, pcov = scipy.optimize.curve_fit(func, SIZE_LIST, avg_insertion_list)
-# popt2, pcov2 = scipy.optimize.curve_fit(func, SIZE_LIST, avg_binary_insertion)
+    #To ensure it is randomized the best it can, the key is randomized for each iteration of our test
+    linkedListBinary = timeit.repeat(lambda: binarySearchList(head, randArray.get(random.randint(0, size - 1))), repeat=ITERATIONS, number=1)
+    arrayBinary = timeit.repeat(lambda: binarySearchArray(randArray, 0, randArray.len() - 1, randArray.get(random.randint(0, size - 1))), repeat=ITERATIONS, number=1)
 
-# plt.scatter(INPUT_SIZE, avg_linear, label='Linked List')
-# plt.scatter(INPUT_SIZE, avg_quick_binary, label='Array')
+    #Sum up the times for each input size and divides it by the # of times it was ran
+    avgLinkedList.append(sum(linkedListBinary) / ITERATIONS)
+    avgArray.append(sum(arrayBinary) / ITERATIONS)
 
-# # Plot the fitted curve
-# x_values = np.linspace(min(SIZE_LIST), max(SIZE_LIST), 100)
-# fitted_curve = func(x_values, *popt)
-# fitted_curve2 = func(x_values, *popt2)
-    
-# Plotting the data and the fitted curve
-# plt.plot(x_values, fitted_curve, 'b')
-# plt.plot(x_values, fitted_curve2, 'r')
+#6)
+# Plotting and curve fitting average time for Arrays
+popt, pcov = scipy.optimize.curve_fit(func, INPUT_SIZE, avgArray)
+plt.scatter(INPUT_SIZE, avgArray, label='Array')
+x_values = np.linspace(min(INPUT_SIZE), max(INPUT_SIZE), 100)
+fitted_curve = func(x_values, *popt)
+plt.plot(x_values, fitted_curve, 'b')
 
-# plt.title(label='Average Timing With Binary Search on Varying Input Sizes For Linked List VS Array')
-# plt.xlabel('Input Sizes')
-# plt.ylabel('Average Time (s)')
-# plt.legend(loc='upper right')
-# plt.show()
+# Plotting and curve fitting average time for Linked Lists
+slope, intercept = np.polyfit(INPUT_SIZE, avgLinkedList, 1)
+plt.scatter(INPUT_SIZE, avgLinkedList, label='Linked List')
+linevalues = [slope * x + intercept for x in INPUT_SIZE]
+plt.plot(INPUT_SIZE, linevalues, 'r')
+
+plt.title(label='Performing Binary Search on Varying Input Sizes For Linked List VS Array')
+plt.xlabel('Input Size')
+plt.ylabel('Average Time (s)')
+plt.legend(loc='upper left')
+plt.show()
